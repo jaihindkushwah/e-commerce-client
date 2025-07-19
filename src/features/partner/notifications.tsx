10adf3cf56544144d63a4bc08db5fb38032d224f
ecojayhind <jayhind.k@pharmesys.com>
2025-07-19 18:01:15 +0530

    feat:added order page for admin and partner

--- src/features/partner/notifications.tsx
+++ src/features/partner/notifications.tsx
@@ -1,5 +1,24 @@
+import type { IOrder } from "@/@types/order";
+import { PartnerService } from "@/services/partner.service";
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../auth/context/AuthContext";
 
 function Notifications() {
+  const { user } = useAuthContext();
+  const [unassignedOrders, setUnassignedOrders] = useState<IOrder[]>([]);
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const partnerService = PartnerService.init(user?.token!);
+        const data = await partnerService.getUnassignedOrders();
+        setUnassignedOrders(data);
+      } catch (error) {
+        console.error("Failed to fetch orders", error);
+      }
+    }
+    fetchOrders();
+  },[])
+
   return (
     <div className="min-h-[calc(100vh-100px)] bg-gradient-to-br from-gray-50 to-white p-6">
       <div className="max-w-6xl mx-auto">
